4319ffe7f37d63c937f5b0c9c5d8100c47421c35
knoll3 <kyle.noll@decentlabs.io>
2023-04-20 13:23:00 -0500

    Add documentation to SarcoClient

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { ethers, Signer, Wallet } from "ethers";
 
+/**
+ * Configuration options for the SarcoClient class.
+ */
 export interface SarcoClientConfig {
   signer?: Signer;
   privateKey?: string;
@@ -7,22 +10,28 @@ export interface SarcoClientConfig {
   provider?: ethers.providers.Provider;
 }
 
+/**
+ * The SarcoClient class provides a high-level interface for interacting with the Sarcophagus V2 protocol.
+ */
 export class SarcoClient {
   private signer: Signer;
 
+  /**
+   * Constructs a new SarcoClient instance. The provider defaults to ethers default provider if not
+   * provided.
+   *
+   * @param config - The configuration options for the SarcoClient.
+   * @throws Will throw an error if none of the signer, private key, or mnemonic is provided.
+   */
   constructor(config: SarcoClientConfig) {
+    const provider = config.provider || ethers.getDefaultProvider();
+
     if (config.signer) {
       this.signer = config.signer;
     } else if (config.privateKey) {
-      if (!config.provider) {
-        throw new Error("A provider is required when using a private key");
-      }
-      this.signer = new Wallet(config.privateKey, config.provider);
+      this.signer = new Wallet(config.privateKey, provider);
     } else if (config.mnemonic) {
-      if (!config.provider) {
-        throw new Error("A provider is required when using a mnemonic");
-      }
-      this.signer = Wallet.fromMnemonic(config.mnemonic).connect(config.provider);
+      this.signer = Wallet.fromMnemonic(config.mnemonic).connect(provider);
     } else {
       throw new Error("A signer, private key, or mnemonic must be provided");
     }
@@ -32,5 +41,5 @@ export class SarcoClient {
     return "Hello World";
   }
 
-  // Your SDK methods will go here
+  // SDK methods will go here
 }
