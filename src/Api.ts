b260a9f5dc99ef60910f07bf2d2d54e3e1229556
Kellar <kellar.base09@gmail.com>
2023-04-26 13:49:15 +0100

    Add rewrap

--- src/Api.ts
+++ src/Api.ts
@@ -5,82 +5,141 @@ import { Signer, ethers } from 'ethers';
 // TODO: Get this from the contracts package
 const goerliDiamondAddress = '0x6B84f17bbfCe26776fEFDf5cF039cA0E66C46Caf';
 
+export interface MethodCallOptions {
+    ignoreSafeCall?: boolean;
+    onTxSuccess?: Function;
+}
+
 export interface CreateSarcophagusOptions {
-  ignoreSafeCall?: boolean;
+    ignoreSafeCall?: boolean;
 }
 
 export function Api<T extends new (...args: any[]) => {}>(Base: T) {
-  return class extends Base {
-    signer: Signer;
+    return class extends Base {
+        signer: Signer;
 
-    constructor(...args: any[]) {
-      super(...args);
-      this.signer = args[0]; // Assuming the signer is the first argument
-    }
-
-    async createSarcophagus(
-      sarcoId: string,
-      name: string,
-      maximumRewrapInterval: number,
-      maximumResurrectionTime: number,
-      recipientAddress: string,
-      resurrectionTime: number,
-      threshold: number,
-      creationTime: number,
-      selectedArchaeologists: {
-        publicKey: Uint8Array;
-        archAddress: string;
-        diggingFeePerSecond: number;
-        curseFee: number;
-        v: number;
-        r: string;
-        s: string;
-      }[],
-      arweaveTxId: string,
-      options: CreateSarcophagusOptions = {}
-    ): Promise<ethers.providers.TransactionResponse> {
-      const contract = new ethers.Contract(
-        goerliDiamondAddress,
-        EmbalmerFacet__factory.abi,
-        this.signer
-      );
-
-      const methodName = 'createSarcophagus';
-      const args = [
-        sarcoId,
-        {
-          name,
-          maximumRewrapInterval,
-          maximumResurrectionTime,
-          recipientAddress,
-          resurrectionTime,
-          threshold,
-          creationTime,
-        },
-        selectedArchaeologists,
-        arweaveTxId,
-      ];
-
-      const useSafeCall = options.ignoreSafeCall !== undefined ? options.ignoreSafeCall : true;
-
-      if (useSafeCall) {
-        try {
-          // Check if the transaction will succeed using callStatic
-          await contract.callStatic[methodName](...args);
-
-          // Proceed with the actual transaction if callStatic succeeds
-          const transactionResponse = await contract[methodName](...args);
-          return transactionResponse;
-        } catch (err) {
-          const error = err as Error;
-          console.error(`Error during the safe contract call: ${error.message}`);
-          throw error;
+        constructor(...args: any[]) {
+            super(...args);
+            this.signer = args[0]; // Assuming the signer is the first argument
         }
-      } else {
-        // If useSafeCall is set to false, directly call the contract method
-        const transactionResponse = await contract[methodName](...args);
-        return transactionResponse;
-      }
-    }
-  };
-}
+
+        async _methodCall(args: {
+            contract: ethers.Contract,
+            methodName: string,
+            inputArgs: any[],
+            options?: MethodCallOptions
+        }): Promise<ethers.providers.TransactionResponse> {
+            const { methodName, inputArgs, options, contract } = args;
+            const useSafeCall = options?.ignoreSafeCall ?? true;
+
+            if (useSafeCall) {
+                try {
+                    // Check if the transaction will succeed using callStatic
+                    await contract.callStatic[methodName](...inputArgs);
+
+                    // Proceed with the actual transaction if callStatic succeeds
+                    const transactionResponse: ethers.ContractTransaction = await contract[methodName](...inputArgs);
+                    transactionResponse.wait().then((reciept) => {
+                        if (options?.onTxSuccess) {
+                            options.onTxSuccess(reciept);
+                        }
+                    });
+                    return transactionResponse;
+                } catch (err) {
+                    const error = err as Error;
+                    console.error(`Error during the safe contract call: ${error.message}`);
+                    throw error;
+                }
+            } else {
+                // If useSafeCall is set to false, directly call the contract method
+                const transactionResponse = await contract[methodName](...inputArgs);
+                return transactionResponse;
+            }
+        }
+
+        async createSarcophagus(
+            sarcoId: string,
+            name: string,
+            maximumRewrapInterval: number,
+            maximumResurrectionTime: number,
+            recipientAddress: string,
+            resurrectionTime: number,
+            threshold: number,
+            creationTime: number,
+            selectedArchaeologists: {
+                publicKey: Uint8Array;
+                archAddress: string;
+                diggingFeePerSecond: number;
+                curseFee: number;
+                v: number;
+                r: string;
+                s: string;
+            }[],
+            arweaveTxId: string,
+            options: CreateSarcophagusOptions = {}
+        ): Promise<ethers.providers.TransactionResponse> {
+            const contract = new ethers.Contract(
+                goerliDiamondAddress,
+                EmbalmerFacet__factory.abi,
+                this.signer
+            );
+
+            const methodName = 'createSarcophagus';
+            const args = [
+                sarcoId,
+                {
+                    name,
+                    maximumRewrapInterval,
+                    maximumResurrectionTime,
+                    recipientAddress,
+                    resurrectionTime,
+                    threshold,
+                    creationTime,
+                },
+                selectedArchaeologists,
+                arweaveTxId,
+            ];
+
+            const useSafeCall = options.ignoreSafeCall !== undefined ? options.ignoreSafeCall : true;
+
+            if (useSafeCall) {
+                try {
+                    // Check if the transaction will succeed using callStatic
+                    await contract.callStatic[methodName](...args);
+
+                    // Proceed with the actual transaction if callStatic succeeds
+                    const transactionResponse = await contract[methodName](...args);
+                    return transactionResponse;
+                } catch (err) {
+                    const error = err as Error;
+                    console.error(`Error during the safe contract call: ${error.message}`);
+                    throw error;
+                }
+            } else {
+                // If useSafeCall is set to false, directly call the contract method
+                const transactionResponse = await contract[methodName](...args);
+                return transactionResponse;
+            }
+        }
+
+        async rewrapSarcophagus(
+            sarcoId: string,
+            newResurrectionTimestampSeconds: number,
+            options?: MethodCallOptions
+        ): Promise<ethers.providers.TransactionResponse> {
+            return this._methodCall({
+                contract: new ethers.Contract(
+                    goerliDiamondAddress,
+                    EmbalmerFacet__factory.abi,
+                    this.signer
+                ),
+                methodName: 'rewrapSarcophagus',
+                inputArgs: [
+                    sarcoId,
+                    newResurrectionTimestampSeconds
+                ],
+                options
+            });
+        }
+    };
+} 
--- src/SarcoClient.ts
+++ src/SarcoClient.ts
@@ -3,10 +3,6 @@ import { getNetwork } from './helpers/getNetwork';
 import { getSigner } from './helpers/getSigner';
 import { Api } from './Api';
 import { SarcoClientConfig } from './types';
-import * as api from './api';
-import * as utils from './utils';
-import * as bundlr from './bundlr';
-import * as archaeologist from './archaeologist';
 
 /**
  * The SarcoClient class provides a high-level interface for interacting with the Sarcophagus V2 protocol.
