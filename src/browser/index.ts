a4a1dd81d5a6c36aae1ae499f2f6236410fa4b7b
Seth Hrbek <seth.hrbek@gmail.com>
2023-05-31 13:20:44 -0400

    finish typings update

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -1,2 +1,3 @@
 import { WebSarcoClient } from './WebSarcoClient';
 export const sarco = new WebSarcoClient();
+export { goerliNetworkConfig, mainnetNetworkConfig, sepoliaNetworkConfig } from "../shared/networkConfig";
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { WebSarcoClient } from "./browser/WebSarcoClient";
+export { goerliNetworkConfig, mainnetNetworkConfig, sepoliaNetworkConfig } from "./shared/networkConfig";
+export { sarco } from "./browser/index";
+export type { ArchaeologistData, ArchaeologistEncryptedShard, SarcophagusArchaeologist, ArchaeologistException } from './shared/types/archaeologist';
+export type { SarcoNetworkConfig } from './shared/types/index';
