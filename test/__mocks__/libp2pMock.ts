a7f11b8aa822146e9b8631fd38d76ec84e47ba12
knoll3 <kyle.noll@decentlabs.io>
2023-06-06 15:09:06 -0500

    Fix libp2p error in tests

---
+++ test/__mocks__/libp2pMock.ts
@@ -0,0 +1,6 @@
+// https://chat.openai.com/share/f612708b-692b-487f-861b-7081f8bec5c5
+const Libp2p = jest.fn().mockImplementation(() => {
+  // add your custom implementation here or just leave it as is
+});
+
+export default Libp2p;
